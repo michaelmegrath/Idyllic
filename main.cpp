4f2e48f3583ff4faaf8cafef4ecf2be5ce7ae066
Michael Megrath <michaelmegrath25@gmail.com>
2019-06-14 16:06:22 -0700

    Inital commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <SDL2/SDL.h>
+#include "initializer.h"
+
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING); //Put in Initalizer
+
+    InitializeIdyllic engine;
+    SDL_Event windowEvent;
+    
+    
+    Uint32 starting_tick; // Put in Initalizer
+    bool running = true;
+    while(running){
+        starting_tick = SDL_GetTicks(); // Put in Initalizer
+        if(SDL_PollEvent(&windowEvent)){ // Put in EventHandler
+            if(SDL_QUIT == windowEvent.type){
+                running = false;
+                break;
+            }
+        }
+        engine.capFps(starting_tick);
+        
+    }
+    engine.~InitializeIdyllic();
+    return EXIT_SUCCESS;
+}
